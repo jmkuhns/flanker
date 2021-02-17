564239cb95e29e65229be708f7abdbe1d2dcbb4e
jmkuhns <jmkuhns@uncg.edu>
2021-02-17 17:04:15 -0500

    updates

--- flanker_main.js
+++ flanker_main.js
@@ -3,7 +3,7 @@
 //var repo_site = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/ArrowFlankerBitmaps";
 //var repo_site = "https://jmkuhns.github.io/flanker/";
 var timeline = [];
-var flanker_bitmaps = "https://jmkuhns.github.io/flanker/ArrowFlankersBitmaps/";
+// var flanker_bitmaps = "https://jmkuhns.github.io/flanker/ArrowFlankersBitmaps/";
 
 /*set up welcome block*/
 var welcome = {
